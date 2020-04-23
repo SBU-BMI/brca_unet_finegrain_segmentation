c29273f0d24239c08e97edc917beea912a6c5fd5
hansbu <ledinhhan2812@gmail.com>
2020-04-23 11:23:52 -0400

    generate json for all WSIs

--- util_codes/__init__.py
+++ util_codes/__init__.py
@@ -1,3 +1,6 @@
 from .gen_annot_predict_pair import main_gen_annot_predict_pair
 from .color_comp_predict import color_comp_main
 from .eval import eval_net
+from .patch_extraction import patch_extraction, data_loader_WSI
+from .predict_WSI import predict_WSI
+from .generate_polygon_json import generate_polygon_json
